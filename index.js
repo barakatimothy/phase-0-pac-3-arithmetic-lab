daef7a3c612168093576e90b82f3774770a3a877
barakatimothy <timothybaraka39@gmail.com>
2023-06-09 15:02:43 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a,b){
+   return a + b;
+}
+add(10,15);
+function subtract(x,y){
+    return x - y;
+}
+subtract(60,40);
+function multiply(c,d){
+    return c * d;
+}
+multiply(3,10);
+function divide(e,f){
+    return e/f; 
+}
+divide(4,2);
+function increment(n)
+{
+    return n+1 ;
+}
+function decrement(n)
+{
+    return n-1 ;
+}
+function makeInt(n)
+{  
+   //assumes base 10:
+    return n;
+}
+function preserveDecimal(n){
+    return n;
+}
+
+
